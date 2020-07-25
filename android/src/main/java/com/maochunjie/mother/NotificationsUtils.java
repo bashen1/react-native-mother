115c8830f48eb4566ab78aa675f8625c0a257cf4
bashen1 <maochunjie@gmail.com>
2020-07-25 14:49:33 +0800

    修复较新安卓系统下获取系统推送开关状态不正确的问题

--- android/src/main/java/com/maochunjie/mother/NotificationsUtils.java
+++ android/src/main/java/com/maochunjie/mother/NotificationsUtils.java
@@ -1,48 +1,75 @@
 package com.maochunjie.mother;
 
+import android.app.AppOpsManager;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationManagerCompat;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-import android.annotation.SuppressLint;
-import android.app.AppOpsManager;
-import android.content.Context;
-import android.content.pm.ApplicationInfo;
-
-@SuppressLint({"NewApi", "InlinedApi"})
 public class NotificationsUtils {
-    private static final String CHECK_OP_NO_THROW = "checkOpNoThrow";
-    private static final String OP_POST_NOTIFICATION = "OP_POST_NOTIFICATION";
-
-    @SuppressLint("InlinedApi")
     public static boolean isNotificationEnabled(Context context) {
-        AppOpsManager mAppOps = (AppOpsManager) context
-                .getSystemService(Context.APP_OPS_SERVICE);
+        NotificationManagerCompat.from(context).areNotificationsEnabled();
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+            return true;
+        }
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            return isEnableV19(context);
+        } else {
+            return isEnableV26(context);
+        }
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private static boolean isEnableV19(Context context) {
+        final String CHECK_OP_NO_THROW = "checkOpNoThrow";
+        final String OP_POST_NOTIFICATION = "OP_POST_NOTIFICATION";
+        AppOpsManager mAppOps = (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
         ApplicationInfo appInfo = context.getApplicationInfo();
         String pkg = context.getApplicationContext().getPackageName();
         int uid = appInfo.uid;
-        Class<?> appOpsClass = null; /* Context.APP_OPS_MANAGER */
+        Class appOpsClass = null; /* Context.APP_OPS_MANAGER */
         try {
             appOpsClass = Class.forName(AppOpsManager.class.getName());
-            Method checkOpNoThrowMethod = appOpsClass
-                    .getMethod(CHECK_OP_NO_THROW, Integer.TYPE, Integer.TYPE,
-                            String.class);
-            Field opPostNotificationValue = appOpsClass
-                    .getDeclaredField(OP_POST_NOTIFICATION);
-            int value = (Integer) opPostNotificationValue.get(Integer.class);
-            return ((Integer) checkOpNoThrowMethod.invoke(mAppOps, value, uid,
-                    pkg) == AppOpsManager.MODE_ALLOWED);
+            Method checkOpNoThrowMethod = appOpsClass.getMethod(CHECK_OP_NO_THROW, Integer.TYPE, Integer.TYPE, String.class);
+            Field opPostNotificationValue = appOpsClass.getDeclaredField(OP_POST_NOTIFICATION);
+            int value = (int) opPostNotificationValue.get(Integer.class);
+            return ((int) checkOpNoThrowMethod.invoke(mAppOps, value, uid, pkg) == AppOpsManager.MODE_ALLOWED);
         } catch (ClassNotFoundException e) {
-            e.printStackTrace();
         } catch (NoSuchMethodException e) {
-            e.printStackTrace();
         } catch (NoSuchFieldException e) {
-            e.printStackTrace();
         } catch (InvocationTargetException e) {
-            e.printStackTrace();
         } catch (IllegalAccessException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
         }
         return false;
+    }
+
+
+    private static boolean isEnableV26(Context context) {
+        ApplicationInfo appInfo = context.getApplicationInfo();
+        String pkg = context.getApplicationContext().getPackageName();
+        int uid = appInfo.uid;
+        try {
+            NotificationManager notificationManager = (NotificationManager)
+                    context.getSystemService(Context.NOTIFICATION_SERVICE);
+            Method sServiceField = notificationManager.getClass().getDeclaredMethod("getService");
+            sServiceField.setAccessible(true);
+            Object sService = sServiceField.invoke(notificationManager);
+
+            Method method = sService.getClass().getDeclaredMethod("areNotificationsEnabledForPackage"
+                    , String.class, Integer.TYPE);
+            method.setAccessible(true);
+            return (boolean) method.invoke(sService, pkg, uid);
+        } catch (Exception e) {
+            return true;
+        }
     }
 }
